e411a720f6f108c3eddf2c2e059d9e28a7d21561
Louis Acheampong <louisacheampong666@gmail.com>
2023-03-02 00:13:42 -0800

    Push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,16 @@
 #include "main.h"
-/**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
- *
- * Return: void
- */
+
 void reverse_array(int *a, int n)
 {
-	int i;
-	int t;
+	int i, j, temp;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
-
